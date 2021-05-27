8b3452a86974ff82ba2844dca59c62653e7266b8
Casper Rogild Storm <casper+github@rogildstorm.com>
2021-05-27 17:29:56 +0200

    middleware package for tukui

--- crates/source/src/tukui.rs
+++ crates/source/src/tukui.rs
@@ -1,6 +1,19 @@
 use super::*;
 use async_trait::async_trait;
 use isahc::prelude::*;
+use serde::{Deserialize, Serialize};
+
+#[derive(Deserialize, Serialize, Clone, Debug)]
+struct Package {
+    name: String,
+}
+
+impl From<Package> for Addon {
+    fn from(item: Package) -> Self {
+        Addon { name: item.name }
+    }
+}
+
 pub struct Tukui {}
 
 #[async_trait]
@@ -8,7 +21,8 @@ impl Source for Tukui {
     async fn get_addons(&self) -> Result<Vec<Addon>, SourceError> {
         let mut response =
             isahc::get_async("https://www.tukui.org/api.php?classic-tbc-addons=all").await?;
-        let addons = response.json::<Vec<Addon>>().await?;
+        let packages = response.json::<Vec<Package>>().await?;
+        let addons = packages.into_iter().map(Addon::from).collect();
         Ok(addons)
     }
 }
