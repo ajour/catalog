2ff77ffebf38eef1f47373b41ee206e4b72bef96
Casper Rogild Storm <casper+github@rogildstorm.com>
2021-05-27 17:37:32 +0200

    removed unwrap.

--- crates/cli/src/main.rs
+++ crates/cli/src/main.rs
@@ -1,20 +1,21 @@
 use futures::executor::block_on;
 use source::tukui::Tukui;
-use source::Source;
+use source::{Error, Source};
 use structopt::StructOpt;
 
 fn main() {
     let future = handle_opts();
-    block_on(future);
+    let _ = block_on(future);
 }
 
-async fn handle_opts() {
+async fn handle_opts() -> Result<(), Error> {
     let opts = Opts::from_args();
     match opts.command {
         Command::Catalog => {
             let tuk = Tukui {};
-            let addons = tuk.get_addons().await.unwrap();
+            let addons = tuk.get_addons().await?;
             dbg!("tukui addons: {}", addons);
+            Ok(())
         }
     }
 }
