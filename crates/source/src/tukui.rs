edf2f9472d19d25197e0c5fae631eea20da98bfa
Casper Rogild Storm <casper+github@rogildstorm.com>
2021-05-27 16:56:52 +0200

    removed old TODO comment

--- crates/source/src/tukui.rs
+++ crates/source/src/tukui.rs
@@ -6,10 +6,8 @@ pub struct Tukui {}
 #[async_trait]
 impl Source for Tukui {
     async fn get_addons(&self) -> Result<Vec<Addon>, SourceError> {
-        // Convert isahc error -> sourceerror.
         let mut response =
             isahc::get_async("https://www.tukui.org/api.php?classic-tbc-addons=all").await?;
-
         let addons = response.json::<Vec<Addon>>().await?;
         Ok(addons)
     }
